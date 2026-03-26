1a830182dad8b241ae6b2594883657b9d1fa9cea
RishavK1811 <rishavkumar181101@gmail.com>
2026-03-26 17:51:01 +0530

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // AI Knowledge Base
     const kb = {
         'hey':"Hello there! I'm Rishav's AI assistant. How can I help you today?",
+        'hii':"Hello there! I'm Rishav's AI assistant. How can I help you today?",
         'hi':"Hello there! I'm Rishav's AI assistant. How can I help you today?",
         'greeting': "Hello there! I'm Rishav's AI assistant. How can I help you today?",
         'whoami': "I'm an AI assistant created by Rishav to help answer questions about his portfolio and background. Ask me anything!",
